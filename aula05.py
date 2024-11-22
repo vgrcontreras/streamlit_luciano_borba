f5d32db6878bd0d468f2fd49bfc7685feb0c6f6b
Victor Contreras <contreras3991@gmail.com>
2024-11-22 11:37:56 -0300

    commit aula 05

---
+++ aula05.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title='Aula 05 - Audio, Imagens e Vídeos',
+    page_icon='assets/img/favicon-32x32.png'
+)
+
+st.title('Julinha do tradelane')
+
+st.image('assets/img/julia.png')
